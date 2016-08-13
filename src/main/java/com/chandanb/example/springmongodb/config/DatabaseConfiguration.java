80723112ed01280b5a1d3aeb5250776ab808f380
meetchandan <chandan00104@gmail.com>
2016-08-13 17:02:57 +0530

    |Adding DB Configuration|

---
+++ src/main/java/com/chandanb/example/springmongodb/config/DatabaseConfiguration.java
@@ -0,0 +1,51 @@
+package com.chandanb.example.springmongodb.config;
+
+
+import com.mongodb.Mongo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.inject.Inject;
+
+@Configuration
+@EnableMongoRepositories("com.chandanb.example.springmongodb.repository")
+@Import(value = MongoAutoConfiguration.class)
+public class DatabaseConfiguration extends AbstractMongoConfiguration  {
+
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    @Inject
+    private Mongo mongo;
+
+    @Inject
+    private MongoProperties mongoProperties;
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return mongoProperties.getDatabase();
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return mongo;
+    }
+}
