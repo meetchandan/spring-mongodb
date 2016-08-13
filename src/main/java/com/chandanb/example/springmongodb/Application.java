87eec8fb68532aa1dedaab81cd3a85346615d06a
meetchandan <chandan00104@gmail.com>
2016-08-13 15:09:33 +0530

    Adding profile support to Application class

--- src/main/java/com/chandanb/example/springmongodb/Application.java
+++ src/main/java/com/chandanb/example/springmongodb/Application.java
@@ -1,19 +1,33 @@
 package com.chandanb.example.springmongodb;
 
+import com.chandanb.example.springmongodb.config.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 
+import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import static com.chandanb.example.springmongodb.config.Constants.DEV_PROFILE;
 
 @SpringBootApplication
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws UnknownHostException {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        System.out.println("Welcome to Spring Boot");
+        SpringApplication app = new SpringApplication(Application.class);
+        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+        app.run(args);
+        System.out.println("Welcome to Spring Mongo DB Example!");
+    }
+
+    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+        if (!source.containsProperty("spring.profiles.active")) {
+            app.setAdditionalProfiles(DEV_PROFILE);
+        }
     }
 }
---
+++ src/main/java/com/chandanb/example/springmongodb/config/Constants.java
@@ -0,0 +1,6 @@
+package com.chandanb.example.springmongodb.config;
+
+public final class Constants {
+    public static final String DEV_PROFILE = "dev";
+
+}
