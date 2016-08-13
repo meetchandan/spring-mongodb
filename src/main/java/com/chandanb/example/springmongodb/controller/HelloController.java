80369ef6431646a34f61058b9e90d8cdeaf8269f
meetchandan <chandan00104@gmail.com>
2016-08-13 11:12:31 +0530

    |Added test and code for a sample HelloController|

---
+++ src/main/java/com/chandanb/example/springmongodb/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.chandanb.example.springmongodb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
